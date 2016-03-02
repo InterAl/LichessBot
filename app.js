2b32270669926672f684a0cacc7f3257966a0a14
interal <interal@gmail.com>
2016-03-02 22:20:47 +0200

    stockfish-query

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 ﻿var http = require('http');
 var child_process = require('child_process');
 
-var white = process.argv[2] == "w";
+var isWhite = process.argv[2] == "w";
 
 function evaluate(fen) {
   console.log("cogitating...");
-  var output = child_process.execSync("node test \"" + fen + "\"", { encoding: "utf8" });
+  var output = child_process.execSync("node stockfish-query \"" + fen + "\"", { encoding: "utf8" });
   var matches = /bestmove ([a-z][0-9][a-z][0-9])/.exec(output);
   var move = matches[1];
   console.log("suggested move:", move);
@@ -32,9 +32,9 @@ http.createServer(function (req, res) {
 	  var move = post.move;
 	  if (!move) return res.end("{}");
 	  var isWhiteMove = move.ply % 2 == 0;
-	  var fen = move.fen + (white ? " w" : " b");
+	  var fen = move.fen + (isWhite ? " w" : " b");
 	  console.log("fen", fen);
-	  if ((!isWhiteMove || white) && (isWhiteMove || !white)) {
+	  if ((!isWhiteMove || isWhite) && (isWhiteMove || !isWhite)) {
 	    var move = evaluate(fen);
 	    res.end(JSON.stringify(move));
 	  } else res.end("{}");
